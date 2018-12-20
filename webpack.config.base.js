d9b92276f7c77e08bfdef4f90a0c0985e64cc890
Mark Ireland <mark.ireland@nearform.com>
2018-12-20 22:57:15 +0000

    Separate Dev and Prod webpack scripts

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
